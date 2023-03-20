1bf468784d4601cbff84b16db6fa2daf7f6ff174
Ram81 <ramramrakhya81@gmail.com>
2023-03-20 00:42:43 -0400

    Add val episodes

--- main.py
+++ main.py
@@ -1,24 +1,52 @@
-
-
 import gzip
-
-from tqdm import tqdm
+from typing import Literal, Optional, Set
 
 import prior
 
 try:
-    from prior import NoCacheLazyJsonDataset
+    from prior import LazyJsonDataset
 except:
     raise ImportError("Please update the prior package (pip install --upgrade prior).")
 
+VALID_SCENE_DATASETS = {"procthor-attr-onav"}
 
-def load_dataset() -> prior.DatasetDict:
+
+#%%
+def load_dataset(
+    minival: bool = False,
+    scene_datasets: Optional[
+        Set[Literal["procthor-attr-onav"]]
+    ] = None,
+) -> prior.DatasetDict:
     """Load the houses dataset."""
+    assert isinstance(minival, bool)
+
+    if scene_datasets is None:
+        scene_datasets = VALID_SCENE_DATASETS
+    else:
+        for scene_dataset in scene_datasets:
+            assert scene_dataset in VALID_SCENE_DATASETS
+    scene_datasets = sorted(scene_datasets)
+
     data = {}
-    for split, size in [("train", 500), ("val", 100)]:
-        with gzip.open(f"{split}.jsonl.gz", "r") as f:
-            houses = [line for line in tqdm(f, total=size, desc=f"Loading {split}")]
-        data[split] = NoCacheLazyJsonDataset(
-            data=houses, dataset="attr-onav-one-room", split=split
+    for split in ["val"]:
+        lines = []
+        used_datasets = []
+        for scene_dataset in scene_datasets:
+            if split == "test" and scene_dataset == "robothor":
+                continue
+
+            with gzip.open(
+                f"{scene_dataset}-locobot-{split}.jsonl.gz", "r"
+            ) as f:
+                lines.extend([line for line in f])
+            used_datasets.append(scene_dataset)
+        if split == "minival":
+            split = "val"
+        data[split] = LazyJsonDataset(
+            data=lines,
+            dataset="|".join(used_datasets) + "|attr-onav-eval",
+            split=split,
         )
+
     return prior.DatasetDict(**data)
